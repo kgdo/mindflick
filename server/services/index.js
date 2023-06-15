1e31c479f692422b79be3ecf5504972a2f58e39b
Ken Dolman <hatch.records@gmail.com>
2023-06-15 12:48:54 +0200

    updating to try and deploy again

---
+++ server/services/index.js
@@ -0,0 +1,4 @@
+module.exports.authService = require('./auth.services');
+module.exports.userService = require('./user.services');
+module.exports.emailService = require('./email.service');
+module.exports.articlesService = require('./articles.service')
